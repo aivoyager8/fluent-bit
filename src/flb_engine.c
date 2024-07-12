4fb5be6f6219a3b8cebdc1cb3cecc90f3b1d8425
aivoyager8 <159749238+aivoyager8@users.noreply.github.com>
2024-07-12 17:19:34 +0800

    set output task no blocking. (#4)
    Use non-blocking writes for output tasks
    
    https://github.com/fluent/fluent-bit/pull/2672

--- src/flb_engine.c
+++ src/flb_engine.c
@@ -777,6 +777,15 @@ int flb_engine_start(struct flb_config *config)
         return -1;
     }
 
+    // chenhg add begin
+    ret = flb_pipe_set_nonblocking(config->ch_manager[1]);
+    if (ret == -1) {
+        flb_errno();
+        return -1;
+    }
+    // chenhg add end
+
+
     /* Start the Storage engine */
     ret = flb_storage_create(config);
     if (ret == -1) {
